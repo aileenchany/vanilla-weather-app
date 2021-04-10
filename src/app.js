aa0fe188ce9fe12639d00813933d3bdaa8ccab21
Aileen CM <aileenchany@gmail.com>
2021-04-10 18:08:43 -0400

    Added JS to populate date & time using the OpenWeather API

--- src/app.js
+++ src/app.js
@@ -1,5 +1,26 @@
+function formatDate (timestamp) {
+    let now = new Date(timestamp);
+
+    let hours = now.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = now.getMinutes();
+    if (minutes < 10) {
+    minutes = `0${minutes}`;
+    }
+    let amOrPm = now.getHours() < 12 ? "am" : "pm";
+
+    let days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+    let day = days[now.getDay()];
+
+    let month = now.getMonth() + 1;
+    let date = now.getDate();
+    return `${day}, ${month}/${date}, ${hours}:${minutes} ${amOrPm}`
+
+}
 function displayTemperature(response) {
-    console.log(response.data);
+    
     document.querySelector("#current-temp").innerHTML = Math.round(response.data.main.temp);
     document.querySelector("#city").innerHTML = response.data.name;
     document.querySelector("#country").innerHTML = response.data.sys.country;
@@ -9,6 +30,10 @@ function displayTemperature(response) {
     document.querySelector("#feels-like-temp").innerHTML = Math.round(response.data.main.feels_like)
     document.querySelector("#high-temp").innerHTML = Math.round(response.data.main.temp_max);
     document.querySelector("#low-temp").innerHTML = Math.round(response.data.main.temp_min);
+    //Below is a function that gets the current date & time data from the OpenWeather API.
+    //dt = unixtime GMT (greenwich mean time). It is the number of seconds since Jan 1st, 1970. 
+    //We need to multiply dt by 1000 to turn it into milisecondshis to get the current date and time
+    document.querySelector("#date").innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "f9ed2779c7a88244e3c6c97a1ad830b5";
